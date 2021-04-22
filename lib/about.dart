025ffdeab3a0b58f64ad59649075194a99ce5344
Robin Roy <robinwackerman@gmail.com>
2021-04-22 13:06:03 +0530

    about_Last

--- lib/about.dart
+++ lib/about.dart
@@ -9,35 +9,39 @@ class About extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: ListView(
           children: <Widget>[
-            Text(
-              'In this pandemic situation it is a hepatic task  to reach each and every members of the society.So here we BIGFOOTians started our presence socially.',
-              style: TextStyle(
-                fontFamily: 'NimbusSanL',
-                fontSize: 14,
-                fontWeight: FontWeight.w700,
+            SafeArea(
+              child: Text(
+                '\n\n\nIn this pandemic situation it is a hepatic task  to reach each and every members of the society.So here we BIGFOOTians started our presence socially.\n\n',
+                style: TextStyle(
+                  fontFamily: 'NimbusSanL',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
             TextButton(
                 child: Text(
                     'LinkedIn : \n https://www.linkedin.com/in/big-foot-5a3229210',
-                    textAlign: TextAlign.justify),
+                    textAlign: TextAlign.left),
                 onPressed: () => linkedIn()),
             TextButton(
                 child: Text(
-                    'Instagram: \n https://www.instagram.com/b_i_g_foot?r=nametag',
-                    textAlign: TextAlign.justify),
+                    '   Instagram: \n   https://www.instagram.com/b_i_g_foot?r=nametag',
+                    textAlign: TextAlign.left),
                 onPressed: () => instagram()),
             TextButton(
                 child: Text(
-                    'Facebook page: \n https://www.facebook.com/Big-Foot-101808942055198/',
-                    textAlign: TextAlign.justify),
+                    '       Facebook page: \nhttps://www.facebook.com/Big-Foot-101808942055198/',
+                    textAlign: TextAlign.left),
                 onPressed: () => instagram()),
-            Text(
-              'BIGFOOT are actually a problem solution firm working as an intern for BMW. Evenif we carry over an automobile intern, Our 9 membered team are so enthusiastic and determined to reach the unreach. Feel free to reach us either through social networks or \nemail us : bigfootmea@gmail.com\n\nPhone us : 7592868515',
-              style: TextStyle(
-                fontFamily: 'NimbusSanL',
-                fontSize: 14,
-                fontWeight: FontWeight.w700,
+            SafeArea(
+              child: Text(
+                '\n\nBIGFOOT are actually a problem solution firm working as an intern for BMW. Evenif we carry over an automobile intern, Our 9 membered team are so enthusiastic and determined to reach the unreach. \nFeel free to reach us either through social networks or \n\nemail us : bigfootmea@gmail.com\n\nPhone us : 7592868515',
+                style: TextStyle(
+                  fontFamily: 'NimbusSanL',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
           ],
